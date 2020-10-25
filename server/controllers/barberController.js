6de0b138c1a83b7e3e9d709ab7a97a6d6c6d2f79
Rueben <rueben.cumberbatch@jdplc.com>
2020-10-25 18:19:17 +0000

    docs(barber): document the createBarber function

--- server/controllers/barberController.js
+++ server/controllers/barberController.js
@@ -1,6 +1,14 @@
-const { Model } = require('mongoose');
 const Models = require('../models/index');
 
+/**
+ * Creates a new barber at a shop based on the
+ * shop ID.
+ *
+ * @function
+ * @param {String} body.name - The name of the new barber.
+ * @param {String} body.shopID - The Shop ID of the Barbershop.
+ * @param {Object} response - HTTP Response object containing updated shop.
+ */
 const createBarber = async ({ body: { name, shopID }}, response) => {
   try {
     const newBarber = await Models.Barber.create({ name: name });
